c5000bccbdecf573ce130596a38be9a8e233ee46
umanath1301 <ms774656@gmail.com>
2021-05-31 16:19:29 +0530

    Second commit

---
+++ image/src/image/Msd.java
@@ -0,0 +1,12 @@
+package image;
+
+public class Msd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String msd= "who is umanth";
+System.out.println(msd);
+
+	}
+
+}
